c4143438e0bae812ca596add4da7e7e5bca9cd87
BrennoDornelasMF <bdmf@cesar.school>
2026-03-02 17:41:53 -0300

    ex5 ok

---
+++ ficha_1/vetor_Matriz_Menor_Soma_Ponderada.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int menorSomaPonderada(int N, double M[][N], double P[]);
+
+int main() {
+    int N = 3;
+
+    double M[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    double P[3] = {1, 2, 3};
+
+    int resultado = menorSomaPonderada(N, M, P);
+
+    printf("Indice da coluna com menor soma ponderada: %d\n", resultado);
+
+    return 0;
+}
+
+
+int menorSomaPonderada(int N, double M[][N], double P[]) {
+    int j, i;
+    
+    int indiceMenor = 0;
+    double menorSoma = 0.0;
+
+    // Calcula a soma da coluna 0 para inicializar
+    for (i = 0; i < N; i++) {
+        menorSoma += M[i][0] * P[i];
+    }
+
+    // Percorre as demais colunas
+    for (j = 1; j < N; j++) {
+        double soma = 0.0;
+
+        for (i = 0; i < N; i++) {
+            soma += M[i][j] * P[i];
+        }
+
+        if (soma < menorSoma) {   // comparação estrita para respeitar empate
+            menorSoma = soma;
+            indiceMenor = j;
+        }
+    }
+
+    return indiceMenor;
+}
+
